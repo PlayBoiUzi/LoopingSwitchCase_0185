a593d741d5cb1d792b7a42e8fae1b792d3c60c89
PlayBoiUzi <dzakyrafi123@gmail.com>
2025-03-17 15:59:54 +0700

    Menambahkan perulangan for pencacah naik

--- contoh_for.cpp
+++ contoh_for.cpp
@@ -5,9 +5,26 @@ using namespace std;
 
 int main()
 {
-    int n:
+    int n;
     int perulanganWhile = 0;
     int perulaganDo = 0;
+    
+    // Perulanagn Dengan for
+
+    cout << "Perulangan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+    cout << "Nilai n = " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
+
+    
+
+
+
+    
+
 
 
 
